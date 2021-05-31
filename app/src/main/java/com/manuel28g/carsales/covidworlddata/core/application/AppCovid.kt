6247b4c78e6a94ba2cb28f1baaff73b286df005e
Manuel Goncalves <man281192@gmail.com>
2021-05-31 10:51:49 -0400

    feat: add Module LifeCycleManager and remove repository folder

--- app/src/main/java/com/manuel28g/carsales/covidworlddata/core/application/AppCovid.kt
+++ app/src/main/java/com/manuel28g/carsales/covidworlddata/core/application/AppCovid.kt
@@ -1,14 +1,18 @@
 package com.manuel28g.carsales.covidworlddata.core.application
 
-import android.app.Application
 import android.content.Context
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
 import au.com.carsales.basemodule.BaseModuleApplication
+import au.com.carsales.basemodule.context
 import au.com.carsales.basemodule.router.BaseModuleLifeCycleManager
 import com.manuel28g.carsales.covidworlddata.core.application.AppCovid.Companion.applicationComponent
+import com.manuel28g.carsales.covidworlddata.module.ModuleLifeCycleManager
 import com.manuel28g.carsales.covidworlddata.ui.di.CovidInfoApplicationModule
 import com.manuel28g.carsales.covidworlddata.ui.di.CovidInfoPlatformComponent
 import com.manuel28g.carsales.covidworlddata.ui.di.DaggerCovidInfoPlatformComponent
+import com.manuel28g.carsalesmobiletestdata.service.CovidDataService
+import com.manuel28g.carsalesmobiletestrouter.covidtestdata.getMobileTestData
 
 open class AppCovid: BaseModuleApplication() {
 
@@ -19,7 +23,7 @@ open class AppCovid: BaseModuleApplication() {
     }
 
     override fun initLifeCycleManager(): BaseModuleLifeCycleManager {
-        return BaseModuleLifeCycleManager(this)
+        return ModuleLifeCycleManager(this)
     }
 
     override fun onCreate() {
@@ -38,6 +42,10 @@ fun Context.appComponent(): CovidInfoPlatformComponent {
 
 fun Fragment.appComponent(): CovidInfoPlatformComponent {
     return buildDagger(this.requireContext().applicationContext)
+}
+
+fun ViewModel.getDataService():CovidDataService?{
+    return context?.getMobileTestData()
 }
 
 private fun buildDagger(context: Context): CovidInfoPlatformComponent {
