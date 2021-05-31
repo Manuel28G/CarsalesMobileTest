8db6002bd96bd2fb77e63cd90be60b60fc250ad1
Manuel Goncalves <man281192@gmail.com>
2021-05-31 10:53:21 -0400

    chore: remove file unused

--- app/src/main/java/com/manuel28g/carsales/covidworlddata/model/CovidInfo.kt
+++
@@ -1,8 +0,0 @@
-package com.manuel28g.carsales.covidworlddata.model
-
-import com.google.gson.annotations.SerializedName
-
-data class CovidInfo (
-        @SerializedName("data")
-        var data: CovidInfoStatus
-        )
