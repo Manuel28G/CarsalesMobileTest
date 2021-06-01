be5329eee538cc1a9e137c975e651c33f50ed81a
Manuel Goncalves <man281192@gmail.com>
2021-05-31 21:52:03 -0400

    refactor: remove code unused

--- app/src/main/java/com/manuel28g/carsales/covidworlddata/core/application/AppCovid.kt
+++ app/src/main/java/com/manuel28g/carsales/covidworlddata/core/application/AppCovid.kt
@@ -3,9 +3,11 @@ package com.manuel28g.carsales.covidworlddata.core.application
 import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
+
 import au.com.carsales.basemodule.BaseModuleApplication
 import au.com.carsales.basemodule.context
 import au.com.carsales.basemodule.router.BaseModuleLifeCycleManager
+
 import com.manuel28g.carsales.covidworlddata.core.application.AppCovid.Companion.applicationComponent
 import com.manuel28g.carsales.covidworlddata.module.ModuleLifeCycleManager
 import com.manuel28g.carsales.covidworlddata.ui.di.CovidInfoApplicationModule
@@ -15,8 +17,6 @@ import com.manuel28g.carsalesmobiletestdata.service.CovidDataService
 import com.manuel28g.carsalesmobiletestrouter.covidtestdata.getMobileTestData
 
 open class AppCovid: BaseModuleApplication() {
-
-    private lateinit var covidInfoPlatformComponent: CovidInfoPlatformComponent
 
     companion object {
         var applicationComponent: CovidInfoPlatformComponent? = null
@@ -31,10 +31,7 @@ open class AppCovid: BaseModuleApplication() {
         appComponent().inject(this)
         buildDagger(this)
     }
-
-    fun getComponent() = covidInfoPlatformComponent
 }
-
 
 fun Context.appComponent(): CovidInfoPlatformComponent {
     return buildDagger(this.applicationContext)
